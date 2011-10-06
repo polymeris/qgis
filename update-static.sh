ebf3bc0675054fc2e41d21e108cf70930047307c
Camilo Polymeris <cpolymeris@gmail.com>
2011-10-05 23:05:22 -0300

    Update static pages

--- update-static.sh
+++ update-static.sh
@@ -1,3 +1,6 @@
 git checkout gh-pages
 markdown ../qgis.wiki/Home.md > body && \
-    cat head body tail > index.html && git commit -m "Update static pages."
+    cat head > index.html && \
+    tail -n +2 body >> index.html && \
+    cat tail >> index.html && \
+    git commit -a -m "Update static pages"
